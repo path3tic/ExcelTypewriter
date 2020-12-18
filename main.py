dd9aff334a51b3ae5834b03cac02a46b2d13729a
Path3tic <zajic@02.pl>
2020-12-18 08:46:53 +0100

    commit

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-
+import pyautogui
+import time
+import keyboard
 import xlwings as xw
 
-def print_hi(name):
-    xw.Book(r'test.xlsb').set_mock_caller()
+#Environment settings
+file = r'test.xlsb'
+mainSH = "BG"
+
+def caller():
+
+    #Getting Source and destination
+    xw.Book(file).set_mock_caller()
     wb = xw.Book.caller()
 
+    #destination
     celldes = wb.app.selection
     rowNum = celldes.row
     colNum = celldes.column
 
-    cellsrc = wb.sheets["BG"].range(rowNum, colNum).formula
+    #source
+    cellsrc = wb.sheets[mainSH].range(rowNum, colNum).formula
 
-    wb.sheets["Sheet1"].range(rowNum, colNum).formula = cellsrc
+    typeit(cellsrc)
+
+
+def typeit(s):
+
+    # Getting Source and destination
+    xw.Book(file).set_mock_caller()
+    wb = xw.Book.caller()
+
+    # destination
+    celldes = wb.app.selection
+    rowNum = celldes.row
+    colNum = celldes.column
+
+    for element in s:
+        pyautogui.write(element)
+    #wb.sheets["Sheet1"].range(rowNum, colNum).formula = s
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    keyboard.add_hotkey('insert', caller)
+    print('ppp')
+    keyboard.wait('insert+esc')
+
+
 
